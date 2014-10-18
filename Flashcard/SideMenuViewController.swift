3788bdf6484f432eed99f10655fa7cc4d6d4ade4
MingEnLee <lee1381@purdue.edu>
2014-10-18 13:05:52 -0400

    TableView Edit

--- Flashcard/SideMenuViewController.swift
+++ Flashcard/SideMenuViewController.swift
@@ -56,17 +56,18 @@ class SideMenuViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
-    override func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
